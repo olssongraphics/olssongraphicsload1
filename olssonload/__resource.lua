7c0bb93e197ce3aae713e8dabb64d731d3905cea
olssongraphics <78671392+olssongraphics@users.noreply.github.com>
2022-04-28 17:59:14 +0200

    Add files via upload

---
+++ olssonload/__resource.lua
@@ -0,0 +1,15 @@
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'script.js',
+    'loadscreen.jpg',
+    'logo.png',
+    'rp.png',
+    'music/loading.mp3'
+   -- 'music/music.ogg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
